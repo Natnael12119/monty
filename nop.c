7ec915fca38bec62d403810e84cd69d6a617e1a5
Natnael12119 <nhattydeveloper@gmail.com>
2023-08-23 07:02:05 -0700

    All mandatory task of 0x19. C - Stacks, Queues - LIFO, FIFO has done.

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- This function did nothing
+  *@head: A stack head
+  *@counter: The line_number
+  *Return: no return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
